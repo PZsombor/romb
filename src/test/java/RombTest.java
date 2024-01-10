621b48a08df26197949c5f5e3350fac67916e6d0
Paulusz Zsombor <paulusz.zsombor@gmail.com>
2024-01-10 11:04:36 +0100

    Teszt10

--- src/test/java/RombTest.java
+++ src/test/java/RombTest.java
@@ -14,10 +14,12 @@ public class RombTest {
     @Test
     public void testCalcArea() {
         double actual = this.romb.calcArea(30.0, 80);
+        double expected = 0.9848;
     }
 
     public void testCalcPerimeter() {
         double actual = this.romb.calcPerimeter();
+        double expected = 120;
     }
 
 
