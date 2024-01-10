5d937e5ed60c1973351cea2b2e4ab15cacd50ee3
Paulusz Zsombor <paulusz.zsombor@gmail.com>
2024-01-10 10:42:32 +0100

    Teszt3

--- src/test/java/RombTest.java
+++ src/test/java/RombTest.java
@@ -1,5 +1,18 @@
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 import lan.zold.Romb;
 
 public class RombTest {
     Romb romb;
+
+    @BeforeTest
+    public void testCalc() {
+        this.romb = new Romb();
+    }
+
+    @Test
+    public void testCalc2() {
+        this.romb.calc();
+    }
 }
